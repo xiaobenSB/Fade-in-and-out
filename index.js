028f3b5488a22b05074299779030e7e0497779cf
xiaobenSB <34466113+xiaobenSB@users.noreply.github.com>
2018-03-20 08:51:49 +0800

    兼容ie10

---
+++ index.js
@@ -0,0 +1,59 @@
+(function() {
+  let fadeFlag = true;
+  function Fade(selector) {
+    this.elem = typeof selector == 'Object' ? selector : document.getElementById(selector);
+  }
+  Fade.prototype = {
+   constructor: Fade,
+    setOpacity: (elem, opacity) => { // 兼容ie10—
+      elem.filters ? elem.style.filter = 'alpha(opacity = '+ opacity +')':  elem.style.opacity = opacity / 100;
+      return true;
+   },
+   setOpacity: function(num) {
+      this.elem.style.opacity !== undefined ? this.elem.style.opacity = num / 100 : this.elem.style.filter = 'alpha(opacity = '+ num +')';
+   },
+    fadeIn: function(speed, opacity){
+   /*
+   speed ==>淡入的速度，正整数（可选）;
+   opacity ==>淡入到指定的透明度，0～100（可选）;
+      */
+      speed = speed || 2;
+      opacity = opacity || 100;
+      let num = 0; // 初始化透明度变化值为0
+      if (fadeFlag) {
+        let time = setInterval(() => {
+          num += speed;
+          fadeFlag = false;
+          this.setOpacity(num);
+          this.elem.style.opacity !== undefined ? this.elem.style.opacity = num / 100 : this.elem.style.filter = 'alpha(opacity = '+ num +')';
+          if (num >= opacity) {
+            clearInterval(time);
+            fadeFlag = true;
+          }
+        }, 20);
+      }
+    },
+    fadeOut: function(speed, opacity) {
+   /*
+   speed ==>淡入的速度，正整数（可选）;
+   opacity ==>淡入到指定的透明度，0～100（可选）;
+   */
+      speed = speed || 2;
+      opacity = opacity || 0;
+      let num = 100; // 初始化透明度变化值为0
+      if (fadeFlag) {
+        let time = setInterval(() => {
+          num -= speed;
+          fadeFlag = false;
+          this.set(num);
+          this.elem.style.opacity !== undefined ? this.elem.style.opacity = num / 100 : this.elem.style.filter = 'alpha(opacity = '+ num +')';
+          if (num <= opacity) {
+          clearInterval(time);
+          fadeFlag = true;
+          }
+        }, 20);
+      }
+    }
+  };
+window.Fade = Fade;
+})();
